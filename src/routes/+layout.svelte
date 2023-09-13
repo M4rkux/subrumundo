821e12cf731b159ff0ad33d8709d21096d5dd1e1
Marcus Amaral Rolim <m4rkux@gmail.com>
2023-09-12 22:45:28 -0700

    Add meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,14 @@
 <!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
+		<meta charset="utf-8"/>
+		<link rel="icon" href="/favicon.ico"/>
+		<meta name="viewport" content="width=device-width,initial-scale=1"/>
+		<meta name="theme-color" content="#000000"/>
+		<meta name="description" content="O melhor podcast de terror do Brasil!"/>
+		<meta property="og:title" content="Subrumundo Terror"><meta property="og:site_name" content="Subrumundo Terror"><meta property="og:description" content="O melhor podcast de terror do Brasil!"><meta property="og:url" content="https://subrumundo.web.app/"><meta property="og:locale" content="pt_BR"><meta property="og:type" content="website"/>
+		<meta property="og:image" content="https://subrumundo.web.app/logo512.png"><meta property="og:image:type" content="image/png"><meta property="og:image:width" content="512"><meta property="og:image:height" content="512"><meta name="robots" content="index, follow, subrumundo, terror, podcast, bruno, contos, histórias, estorias"><link rel="apple-touch-icon" href="logo192.png"/>
+		<title>Subrumundo Terror</title>
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
